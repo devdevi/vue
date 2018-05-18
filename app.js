bdb24bb13eacf4a3dcc27caabbd2f04c6b7f7e3e
Visaka <visidevi@gmail.com>
2018-05-18 09:38:38 -0400

    comentario

--- app.js
+++ app.js
@@ -89,3 +89,19 @@ console.log(platzom(`programar`))
 console.log(platzom(`zorro`))
 console.log(platzom(`zarpar`))
 console.log(platzom(`abcdarios`))
+
+/* function allLongestStrings(inputArray) {
+    var r = [];
+    var mL = 0;
+    for (var i=0; i<inputArray.length; i++) {
+        if (inputArray[i].length > mL) {            
+            r.length = 0;
+            r.push(inputArray[i]);
+            mL = inputArray[i].length;
+        }
+        else if (inputArray[i].length == mL) { 
+            r.push(inputArray[i]);
+        }
+    }
+    return r;
+} */
